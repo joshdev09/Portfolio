916232a2d3111bf11d0e8e827310e1a81e707dbc
Joshua Halili <110602419+joshdev09@users.noreply.github.com>
2026-07-05 19:44:23 +0800

    nav

---
+++ src/assets/components/Nav.tsx
@@ -0,0 +1,15 @@
+function Nav(){
+    return(
+        <div>
+            <nav>
+                <ul>
+                    <li>About</li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav;
